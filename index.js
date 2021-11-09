f8b3ccf34670480304d9b0c4040b946532169285
Mathis Angibaud <mathisangibaud@MacBook-Pro-de-Mathis.local>
2021-11-09 15:40:51 +0100

    Configure gitlabCI eslint

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ const app = express()
 const axios = require('axios')
 
 app.listen(8080, () => {
-    console.log('Serveur l\'écoute')
+    console.log('Serveur démarré')
 })
 
 app.get('/', (req, res) => {
     axios.get('https://api.binance.com/api/v3/ticker/price')
         .then(function (response) {
-            // handle success
             console.log(response);
             res.send(response.data)
         })
         .catch(function (error) {
-            // handle error
             console.log(error);
-        })
-        .then(function () {
-            // always executed
         });
 
 })

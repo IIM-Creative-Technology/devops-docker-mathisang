7ecadb23cd3b37e948428c35af8e93f264ab6404
Mathis Angibaud <mathisangibaud@macbook-pro-de-mathis.home>
2021-11-14 18:42:34 +0100

    update port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Serveur démarré')
 })
 

199ba362c951dc36b6a46bb2c3638aa0691de29d
Mathis Angibaud <mathisangibaud@macbook-pro-de-mathis.home>
2021-11-14 18:17:56 +0100

    tryinh to change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
-app.listen(8080, () => {
+app.listen(5000, () => {
     console.log('Serveur démarré')
 })
 
